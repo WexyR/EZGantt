1c262e9f7e0393aa57338b42f2e00899257560df
ShinySilver <nicolas.louismarie@gmail.com>
2019-06-06 17:45:44 +0200

    Fixed ./build.sh. I commented the browserify line, but it can be added-back if necessary

--- build.sh
+++ build.sh
@@ -1,16 +1,8 @@
 
 cd ./src/ts/
-while true; do
-    read -p "Do you want to recompile this software? [Y/n]  >> " yn
-    if [ -n "$yn" ]; then
-        if [ $yn = "N" ] || [ $yn = "n" ]; then break; fi
-    fi
-    echo "Compilation en cours..."
-    tsc *.ts */*.ts --lib es2015,dom
-    cd bdd
-    browserify bdd.js -o bundle.js
-    cd -
-    echo "Compilation terminée!"
-    echo ""
-done
-cd -
+echo "Compilation en cours..."
+tsc *.ts */*.ts --lib es2015,dom
+cd bdd
+echo Command 'browserify bdd.js -o bundle.js' disabled
+echo "Compilation terminée!"
+
